451e1e734636b9b57688c90ee2c6dc3ef4de01ac
Pavol Antalík <pavol.antalik@gmail.com>
2019-08-25 15:15:04 +0200

    Disabling provisioners until SSH key issues are resolved, route53 entry update

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,22 +11,25 @@ resource "aws_instance" "minecraft_server" {
     Name = "${var.aws_instance_name}"
   }
 
-  connection {
-    user = "ec2-user"
-  }
+  # connection {
+  #   user = "ec2-user"
+  #   private_key = ""
+  # }
 
-  # Copies generated SystemD service file
-  provisioner "file" {
-    content     = "${data.template_file.instance_provisioning.rendered}"
-    destination = "/home/ec2-user/minecraft.service"
-  }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo mv /home/ec2-user/minecraft.service /etc/systemd/system/minecraft.service",
-      "sudo daemon-reload",
-    ]
-  }
+  # # Copies generated SystemD service file
+  # provisioner "file" {
+  #   content     = "${data.template_file.instance_provisioning.rendered}"
+  #   destination = "/home/ec2-user/minecraft.service"
+  # }
+
+
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "sudo mv /home/ec2-user/minecraft.service /etc/systemd/system/minecraft.service",
+  #     "sudo daemon-reload",
+  #   ]
+  # }
 
   # generated script for provision the instance
   user_data = "${base64encode(data.template_file.instance_provisioning.rendered)}"
@@ -34,8 +37,8 @@ resource "aws_instance" "minecraft_server" {
 
 resource "aws_route53_record" "minecraft_server" {
   zone_id = "${aws_route53_zone.minecraft_server.zone_id}"
-  name    = "minecraft_server"
-  type    = "CNAME"
+  name    = "server"
+  type    = "A"
   ttl     = "300"
-  records = ["${aws_instance.minecraft_server.public_dns}"]
+  records = ["${aws_instance.minecraft_server.public_ip}"]
 }
