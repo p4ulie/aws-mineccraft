63ead35a6310be1ecf1198e7046e3fd7b6ba941d
Pavol Antalik <pavol.antalik@gmail.com>
2019-12-15 16:18:07 +0100

    Update default AMI ID, instance type, region (Frankfurt, eu-central-1)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,12 +9,12 @@ variable "ssh_key_name" {
 
 variable "aws_region" {
   description = "AWS region to launch servers."
-  default     = "eu-west-1"
+  default     = "eu-central-1"
 }
 
 variable "aws_instance_type" {
   description = "AWS instance type."
-  default     = "t2.micro"
+  default     = "t3.medium"
 }
 
 variable "aws_instance_name" {
@@ -23,8 +23,8 @@ variable "aws_instance_name" {
 }
 
 variable "ami_default" {
-  description = "Default base AMI"
-  default     = "ami-0bbc25e23a7640b9b"
+  description = "Default base AMI - region specific ID (eu-central-1 Frankfurt)"
+  default     = "ami-00aa4671cbf840d82"
 }
 
 variable "ami_custom" {
@@ -34,7 +34,7 @@ variable "ami_custom" {
 
 variable "s3_bucket_name" {
   description = "Name of S3 bucket to store Minecraft server world backups"
-  default     = "s3-minecraft-p4ulie-net-backup"
+  default     = "s3-minecraft-p4ulie-net-world-backup"
 }
 
 # =============================================================================
@@ -51,6 +51,11 @@ variable "minecraft_user" {
   default     = "minecraft"
 }
 
+variable "minecraft_domain" {
+  description = "Default DNS domain for running Minecraft server"
+  default     = "minecraft.p4ulie.net"
+}
+
 variable "minecraft_directory" {
   description = "Default directory for installing Minecraft server"
   default     = "/opt/minecraft"
@@ -62,8 +67,8 @@ variable "minecraft_java_opts" {
 }
 
 variable "minecraft_download_url" {
-  description = "URL for downloading Minecraft server (1.14.4)"
-  default     = "https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar"
+  description = "URL for downloading Minecraft server (1.15)"
+  default     = "https://launcher.mojang.com/v1/objects/e9f105b3c5c7e85c7b445249a93362a22f62442d/server.jar"
 }
 
 variable "minecraft_server_filename" {
